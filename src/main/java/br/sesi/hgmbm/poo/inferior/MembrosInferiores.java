c217cac84deb582981296b55218b85d7421b4ef6
HannahGrecco <Grecco327@gmail.com>
2024-09-09 12:07:34 -0300

    criando contrutor

--- src/main/java/br/sesi/hgmbm/poo/inferior/MembrosInferiores.java
+++ src/main/java/br/sesi/hgmbm/poo/inferior/MembrosInferiores.java
@@ -1,5 +1,5 @@
 package br.sesi.hgmbm.poo.inferior;
 
 public class MembrosInferiores {
-
+    public Perna perna;
 }
---
+++ src/main/java/br/sesi/hgmbm/poo/inferior/Perna.java
@@ -0,0 +1,8 @@
+package br.sesi.hgmbm.poo.inferior;
+
+public class Perna {
+    public int qntDedosDireita;
+    public int qntdDedosEsquerda;
+    public boolean possui2pernas;
+    public float cmDaPerna;
+}
